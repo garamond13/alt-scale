2cab272dbe4cdbcc4c842e68b139c8410c24ffea
garamond13 <98652255+garamond13@users.noreply.github.com>
2022-12-08 02:22:21 +0100

    update clamp

--- altUpscale.glsl
+++ altUpscale.glsl
@@ -21,7 +21,7 @@
 #define sigmoidize(rgba) (MIDPOINT - log(1.0 / clamp((1.0 / (1.0 + exp(CONTRAST * (MIDPOINT - 1.0))) - 1.0 / (1.0 + exp(CONTRAST * MIDPOINT))) * rgba + 1.0 / (1.0 + exp(CONTRAST * MIDPOINT)), FLT_EPSILON, 1.0 - FLT_EPSILON) - 1.0) / CONTRAST)
 
 vec4 hook() {
-    return sigmoidize(clamp(linearize(textureLod(HOOKED_raw, HOOKED_pos, 0.0)), 0.0, 1.0));
+    return sigmoidize(linearize(clamp(textureLod(HOOKED_raw, HOOKED_pos, 0.0), 0.0, 1.0)));
 }
 
 //!HOOK MAIN
