23e63a687436feccca79e3eb1b142a119431e768
garamond13 <98652255+garamond13@users.noreply.github.com>
2022-12-09 01:24:49 +0100

    simplify

--- altDownscaleGaussian.glsl
+++ altDownscaleGaussian.glsl
@@ -32,8 +32,7 @@ vec4 hook() {
     float wsum = weight;
     for(float i = 1.0; i <= RADIUS; ++i) {
         weight = get_weight(i);
-        csum += textureLod(PASS0_raw, PASS0_pos + PASS0_pt * vec2(0.0, -i), 0.0) * weight;
-        csum += textureLod(PASS0_raw, PASS0_pos + PASS0_pt * vec2(0.0, i), 0.0) * weight;
+        csum += (textureLod(PASS0_raw, PASS0_pos + PASS0_pt * vec2(0.0, -i), 0.0) + textureLod(PASS0_raw, PASS0_pos + PASS0_pt * vec2(0.0, i), 0.0)) * weight;
         wsum += 2.0 * weight;
     }
     return csum / wsum;
@@ -63,8 +62,7 @@ vec4 hook() {
     float wsum = weight;
     for(float i = 1.0; i <= RADIUS; ++i) {
         weight = get_weight(i);
-        csum += textureLod(PASS1_raw, PASS1_pos + PASS1_pt * vec2(-i, 0.0), 0.0) * weight;
-        csum += textureLod(PASS1_raw, PASS1_pos + PASS1_pt * vec2(i, 0.0), 0.0) * weight;
+        csum += (textureLod(PASS1_raw, PASS1_pos + PASS1_pt * vec2(-i, 0.0), 0.0) + textureLod(PASS1_raw, PASS1_pos + PASS1_pt * vec2(i, 0.0), 0.0)) * weight;
         wsum += 2.0 * weight;
     }
     return csum / wsum;
