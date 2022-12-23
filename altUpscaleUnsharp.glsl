1350cb8ece6023ee6e43048d29e8c3ddec58379e
garamond13 <98652255+garamond13@users.noreply.github.com>
2022-12-23 01:10:29 +0100

    remove extra brackets

--- altUpscaleUnsharp.glsl
+++ altUpscaleUnsharp.glsl
@@ -239,7 +239,7 @@ vec4 hook() {
 //
 ////////////////////////////////////////////////////////////////////////
 
-#define get_weight(x) (exp(-(x * x / (2.0 * SIGMA * SIGMA))))
+#define get_weight(x) (exp(-x * x / (2.0 * SIGMA * SIGMA)))
 
 vec4 hook() {
     float weight = get_weight(0.0);
@@ -274,7 +274,7 @@ vec4 hook() {
 //
 ////////////////////////////////////////////////////////////////////////
 
-#define get_weight(x) (exp(-(x * x / (2.0 * SIGMA * SIGMA))))
+#define get_weight(x) (exp(-x * x / (2.0 * SIGMA * SIGMA)))
 
 vec4 hook() {
     float weight = get_weight(0.0);
